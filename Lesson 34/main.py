092ae8105aaf51fcea41e3fc438c315474628c08
KingAJ19 <anelesokwane@gmail.com>
2025-07-24 04:10:00 +0200

    advanced strings

---
+++ Lesson 34/main.py
@@ -0,0 +1,7 @@
+#Advanced concept - Strings
+
+message = ' Hello, World! '
+
+print(message.strip()) #Remove leading and trailing whitespace
+print(message.lower()) #Convert all characters to lowerspace
+print(message.split()) #Split the string into a list based on the comma
