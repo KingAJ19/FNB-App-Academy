003b4aa87af7b8858a3462522b351e460af74ac4
KingAJ19 <anelesokwane@gmail.com>
2025-07-24 09:08:21 +0200

    list

---
+++ Lesson 43/lists.py
@@ -0,0 +1,15 @@
+#lists
+
+fruits = ["apple", "oranges", "cherry"]
+
+fruits.append("kiwi")
+print(fruits)
+
+fruits.insert(1, "banana")
+print(fruits)
+
+fruits.remove("kiwi")
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)
